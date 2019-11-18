998391adeb84136f1151ff499614ace2ec2616a1
tjarrow <nickita0104@gmail.com>
2019-11-18 22:52:05 +0300

    Add application components

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import Scheme from './components/Scheme';
+import './styles/styles.scss';
+import 'normalize.css/normalize.css';
+
+ReactDOM.render(<Scheme/>, document.getElementById('app'));
