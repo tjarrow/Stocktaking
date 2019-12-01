18b2ca5b9d86d4bdead7c35430b66f8743c5b6f7
tjarrow <nickita0104@gmail.com>
2019-12-02 00:49:07 +0300

    Render routing component

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import AppRouter from './routers/AppRouter';
 import Scheme from './components/Scheme';
 import './styles/styles.scss';
 import 'normalize.css/normalize.css';
 
-ReactDOM.render(<Scheme/>, document.getElementById('app'));
+ReactDOM.render(<AppRouter/>, document.getElementById('app'));
