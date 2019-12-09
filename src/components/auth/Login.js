96de42169b3d69b87fe79a6ea7e7f28019b5fda1
tjarrow <nickita0104@gmail.com>
2019-12-09 23:11:06 +0300

    Update Login and Register components
    1. Form handlers added
    2. Input elements changed on TextFieldGroup
    3. Data type checking via prop-types
    4. Set connection to router via react-dom-router

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,24 +1,125 @@
 import React from 'react';
-
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
 class Login extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      email:'',
+      password:'',
+      errors: {}
+    }
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.auth.isAuthenticated) {
+      // Send to the main page
+      this.props.history.push("/view");
+    }
+
+    if (nextProps.errors) {
+      this.setState({ errors: nextProps.errors });
+    }
+  }
+
+  componentDidMount() {
+    if (this.props.auth.isAuthenticated) {
+      // Send to the main page
+      this.props.history.push("/view");
+    }
+  }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    const userData = {
+      email: this.state.email,
+      password: this.state.password
+    };
+    this.props.loginUser(userData);
+  }
+
+ //  handleClick (event) {
+ //     var apiBaseUrl = "http://localhost:8080/api/"; // или localhost:4000 ?
+ //     var self = this;
+ //     var payload = {
+ //       "email": this.state.username,
+ //       "password": this.state.password
+ //     }
+ //
+ //     axios.post(apiBaseUrl + 'login', payload)
+ //     .then(function (response) {
+ //       console.log(response);
+ //       if (response.data.code == 200) {
+ //         console.log("Login successfull");
+ //         // var uploadScreen=[];
+ //         // uploadScreen.push(<UploadScreen appContext={self.props.appContext}/>)
+ //         // self.props.appContext.setState({loginPage:[],uploadScreen:uploadScreen})
+ //     }
+ //     else if (response.data.code == 204) {
+ //       console.log ("Username password do not match");
+ //     } else {
+ //       console.log("Username does not exists");
+ //       }
+ //     }).catch(function (error) {
+ //       console.log(error);
+ //     });
+ // }
+
   render () {
+    const { errors } = this.state;
+
     return (
       <div className="auth-form">
-        <div className="auth-btn-wrapper">
+        <form onSubmit={this.onSubmit} className="auth-btn-wrapper">
             <h2 className="start-header">Войдите в ваш аккаунт Stocktaking</h2>
-            <input
+            <TextFieldGroup
               className="auth-input"
+              name="email"
               placeholder="Введите логин"
+              error={errors.email}
+              value={this.state.email}
+              onChange={this.onChange}
+              type="email"
+            />
+            <TextFieldGroup
+              className="auth-input"
+              name="password"
+              placeholder="Введите пароль"
+              error={errors.password}
+              value={this.state.password}
+              onChange={this.onChange}
+              type="password"
             />
             <input
-              className="auth-input"
-              placeholder="Введите пароль"
+              type="submit"
+              className="auth-submit"
             />
-        </div>
+        </form>
       </div>
     )
   }
 }
 
-export default Login;
+Login.propTypes = { // для проверки корректности типов вводимых данных
+  loginUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth,
+  errors: state.errors
+});
+
+export default connect( //ES6 synthax
+  mapStateToProps,
+  { loginUser } // чтобы вытащить именно одно свойство из класса loginUser
+)(Login);
--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -2,31 +2,104 @@ import React from 'react';
 
 class Register extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state={
+      company_name: '',
+      email: '',
+      password: '',
+      password2: '',
+      errors: {}
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.auth.isAuthenticated) {
+      // Send to the main page
+      this.props.history.push("/view");
+    }
+
+    if (nextProps.errors) {
+      this.setState({ errors: nextProps.errors });
+    }
+  }
+
+  componentDidMount() {
+    if (this.props.auth.isAuthenticated) {
+      // Send to the main page
+      this.props.history.push("/view");
+    }
+  }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    const newUser = {
+      company_name: this.state.company_name,
+      email: this.state.email,
+      password: this.state.password,
+      password2: this.state.password2
+    };
+
+    this.props.registerUser(newUser, this.props.history);
+  }
+
   render () {
     return (
       <div className="auth-form">
-      <div className="auth-btn-wrapper">
+      <form onSubmit={this.onSubmit} className="auth-btn-wrapper">
           <h2 className="start-header">Зарегистрируйтесь в Stocktaking</h2>
-          <input
+          <TextFieldGroup
             className="auth-input"
+            name="company-name"
+            error={errors.company_name}
+            value={this.state.company_name}
+            onChange={this.onChange}
             placeholder="Введите название организации"
           />
-          <input
+          <TextFieldGroup
             className="auth-input"
+            name="email"
+            error={errors.email}
+            value={this.state.email}
+            onChange={this.onChange}
             placeholder="Введите логин"
           />
-          <input
+          <TextFieldGroup
             className="auth-input"
+            error={errors.password}
+            value={this.state.password}
+            onChange={this.onChange}
             placeholder="Введите пароль"
           />
-          <input
+          <TextFieldGroup
             className="auth-input"
+            error={errors.password2}
+            value={this.state.password2}
+            onChange={this.onChange}
             placeholder="Повторите пароль"
           />
-        </div>
+        </form>
       </div>
     )
   }
 }
 
-export default Register;
+Register.propTypes = {
+  registerUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
+};
+
+const mapStateProps = state => ({
+  auth: state.auth,
+  errors: state.errors
+});
+
+export default connect(
+  mapStateProps,
+  { registerUser }
+)(withRouter(Register));
